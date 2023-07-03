660e6340076dc48d27f8b4723e1b49bb457c52f8
Kishor Kumar Naik P <kishor88611@gmail.com>
2023-07-03 21:56:56 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomeException
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            if not os.path.exists('artifacts'):
+                os.makedirs('artifacts')
+
+            df = pd.read_csv(r'C:\Users\kishu\PycharmProjects\ML Project\notebook\Data\stud.csv')
+            print('Read the dataset as a dataframe')
+            logging.info('Read the dataset as a dataframe')
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            print('Raw data saved:', self.ingestion_config.raw_data_path)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            print('Train data saved:', self.ingestion_config.train_data_path)
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            print('Test data saved:', self.ingestion_config.test_data_path)
+
+            logging.info("Ingestion of the data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            print('Error occurred during data ingestion:', e)
+
+data_ingestion = DataIngestion()
+data_ingestion.initiate_data_ingestion()
+
+
