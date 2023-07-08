36fb1164e4b665401c9351c40fc924187b1d9d61
Kishor Kumar Naik P <kishor88611@gmail.com>
2023-07-08 13:49:26 +0530

    Model fintuning

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import dill
 from src.exception import CustomeException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 import numpy as np
 
@@ -19,13 +20,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomeException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train,y_train)
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)      # Hyper parameter tuning
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
@@ -40,5 +49,5 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomeException(e, sys)
 
