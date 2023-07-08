46351bdc606ba3d0c1597a9660c64e6c76b89b4a
Kishor Kumar Naik P <kishor88611@gmail.com>
2023-07-08 13:07:52 +0530

    ModelTraining

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -55,5 +57,8 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr,test_arr,preprocessor_ob_file_path = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 import dill
 from src.exception import CustomeException
-
+from sklearn.metrics import r2_score
 import pandas as pd
 import numpy as np
 
@@ -18,3 +18,27 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomeException(e, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
